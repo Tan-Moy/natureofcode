dcc59bea84b1891ea28c3fb731ff1806dd6829df
Tan Moy <Tan Moy>
2017-07-21 11:26:19 +0530

    The ball got drunk, Had to put it back in  line

--- session1/lib/index.js
+++ session1/lib/index.js
@@ -4,25 +4,41 @@ var w;
 
 //walker constructor function
 function Walker() {
-    
     this.pos = createVector(width / 2, height / 2);
     //create a pos vector replaces the below two lines
     //this.x = width/2;
     //this.y = height/2;
-    this.vel = createVector(random(-3,3),random(-3,3)); //create a velociy vector. Put it in upadte function for the vibration effect
-    this.acc = createVector(0,0.1);
+    this.vel = createVector(0, 0); //create a velociy vector. Put it in upadte function for the vibration effect
 
-    this.display = function () {
-        fill(255);
-        ellipse(this.pos.x, this.pos.y, 48, 48);//ellipse(x,y,radiux_x,radius_y)
-        
-    }
+    //this.acc = createVector(0,0.1);
+    //this.acc = p5.Vector.fromAngle(random(TWO_PI))//create an vector with random angle
 
     this.update = function () {
-        this.vel.add(this.acc);//add accelaration vector to velocity vector
-        this.pos.add(this.vel); //add the position vector to random vector(vel) replacing the below lines
+        var mouse = createVector(mouseX, mouseY);//get the mouse pointer coordinates
+
+        this.acc = p5.Vector.sub(mouse, this.pos);//subtract the mouse and position vector and store the value as acc vector. This is called static. 
+
+
+        //this.acc = createVector(random(-1, 1), random(-1, 1));//put this outside upadte to get smooth falling down effect
+
+        //this.acc.normalize();//normalizes the acc vector's magnitude to 1;
+
+        //this.acc.mult(0.01);//scaling up the acc vector i.e multiplying operator and updating acc. If used with normalize mult sets the mag to be normalised
+
+        //this.acc.rotate(0.1);//rotates the vector 
+
+        this.acc.setMag(0.4);//normalize and mmult in one function.
+
+        this.vel.add(this.acc);//add accelaration vector to velocity vector and upadte vel's value
+        this.pos.add(this.vel); //add the position vector to random vector(vel) and updating pos and replacing the below lines
         // this.pos.x = this.pos.x + random(-3, 3);
         // this.pos.y = this.pos.y + random(-3, 3);
+
+    }
+    this.display = function () {
+        fill(255);
+        ellipse(this.pos.x, this.pos.y, 20, 20);//ellipse(x,y,radiux_x,radius_y)
+
     }
 
 }
